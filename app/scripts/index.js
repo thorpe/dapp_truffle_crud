c3d2a136d5b149fb6b9052c46f7086376072cee0
thorpe <thorpe@spinprotocol.io>
2018-12-14 18:58:41 +0900

    sd
    Signed-off-by: thorpe <thorpe@spinprotocol.io>

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,220 +1,182 @@
 // Import the page's CSS. Webpack will know what to do with it.
-import '../styles/app.css'
+import '../styles/app.css';
 
-import { default as Web3 } from 'web3'
-import { default as contract } from 'truffle-contract'
+import {default as Web3} from 'web3';
+import {default as contract} from 'truffle-contract';
 
-import metaCoinArtifact from '../../build/contracts/MetaCoin.json'
-import crudAppArtifact from '../../build/contracts/CrudApp.json'
+import metaCoinArtifact from '../../build/contracts/MetaCoin.json';
+import crudAppArtifact from '../../build/contracts/CrudApp.json';
 
-const MetaCoin = contract(metaCoinArtifact)
-const CrudApp = contract(crudAppArtifact)
+const MetaCoin = contract(metaCoinArtifact);
+const CrudApp = contract(crudAppArtifact);
 
-let accounts
-let account
+let accounts;
+let account;
 
 const App = {
   start: function () {
-    const self = this
+    const self = this;
 
-    MetaCoin.setProvider(web3.currentProvider)
-    CrudApp.setProvider(web3.currentProvider)
+    MetaCoin.setProvider(web3.currentProvider);
+    CrudApp.setProvider(web3.currentProvider);
 
-    // Get the initial account balance so it can be displayed.
     web3.eth.getAccounts(function (err, accs) {
       if (err != null) {
-        alert('There was an error fetching your accounts.')
-        return
+        alert('There was an error fetching your accounts.');
+        return;
       }
 
       if (accs.length === 0) {
-        alert('Couldn\'t get any accounts! Make sure your Ethereum client is configured correctly.')
-        return
+        alert('Couldn\'t get any accounts! Make sure your Ethereum client is configured correctly.');
+        return;
       }
 
-      accounts = accs
-      account = accounts[0]
+      accounts = accs;
+      account = accounts[0];
 
-      self.refreshBalance()
+      self.refreshBalance();
     })
   },
 
   setStatus: function (message) {
-    const status = document.getElementById('status')
-    status.innerHTML = message
+    const status = document.getElementById('status');
+    status.innerHTML = message;
   },
 
   refreshBalance: function () {
-    const self = this
-
-    let meta
+    const self = this;
     MetaCoin.deployed().then(function (instance) {
-      meta = instance
-      return meta.getBalance.call(account, { from: account })
+      return instance.getBalance.call(account, {from: account});
     }).then(function (value) {
-      const balanceElement = document.getElementById('balance')
-      balanceElement.innerHTML = value.valueOf()
+      const balanceElement = document.getElementById('balance');
+      balanceElement.innerHTML = value.valueOf();
     }).catch(function (e) {
-      console.log(e)
-      self.setStatus('Error getting balance; see log.')
+      console.log(e);
+      self.setStatus('Error getting balance; see log.');
     })
   },
 
   sendCoin: function () {
-    const self = this
+    const self = this;
+    const amount = parseInt(document.getElementById('amount').value);
+    const receiver = document.getElementById('receiver').value;
+    this.setStatus('Initiating transaction... (please wait)');
 
-    const amount = parseInt(document.getElementById('amount').value)
-    const receiver = document.getElementById('receiver').value
-
-    this.setStatus('Initiating transaction... (please wait)')
-
-    let meta
+    let meta;
     MetaCoin.deployed().then(function (instance) {
-      meta = instance
-      return meta.sendCoin(receiver, amount, { from: account })
+      meta = instance;
+      return meta.sendCoin(receiver, amount, {from: account})
     }).then(function () {
-      self.setStatus('Transaction complete!')
-      self.refreshBalance()
+      self.setStatus('Transaction complete!');
+      self.refreshBalance();
     }).catch(function (e) {
-      console.log(e)
-      self.setStatus('Error sending coin; see log.')
+      console.log(e);
+      self.setStatus('Error sending coin; see log.');
     })
   },
 
   doInsert: function () {
-    const self = this
-    const amount = parseInt(document.getElementById('amount').value)
-    const receiver = document.getElementById('receiver').value
-    const countryName = document.getElementById('countryName').value
-    const leader = document.getElementById('leader').value
-    const population = parseInt(document.getElementById('population').value)
+    const self = this;
+    const countryName = document.getElementById('countryName').value;
+    const leader = document.getElementById('leader').value;
+    const population = parseInt(document.getElementById('population').value);
 
-    let crud
-    this.setStatus('Initiating transaction... (please wait)')
     CrudApp.deployed().then(function (instance) {
-      crud = instance
-      crud.doInsert(countryName, leader, population, { from: account })
-      return true
+      instance.doInsert(countryName, leader, population, {from: account});
+      return true;
     }).then(function () {
-      self.setStatus('Transaction complete!')
-      self.refreshBalance()
+      self.setStatus('Transaction complete!');
     }).catch(function (e) {
-      console.log(e)
-      self.setStatus('Error sending coin; see log.')
+      console.log(e);
+      self.setStatus('Error sending coin; see log.');
     })
   },
 
   getOneByCountryName: function () {
-    const self = this
-    const countryName = document.getElementById('countryName').value
-
-    let a
-    let crud
-    this.setStatus('Initiating transaction... (please wait)')
-    CrudApp.deployed().then(function (instance2) {
-      crud = instance2
-      crud.getOneByCountryName(countryName, { from: account })
+    const self = this;
+    const countryName = document.getElementById('countryName').value;
+    CrudApp.deployed().then(function (instance) {
+      instance.getOneByCountryName(countryName, {from: account})
         .then(function (data) {
-          a = data
-          console.log(a)
+          console.log(data);
+          $("#myTable tbody:last").append("<tr id='row_" + data[0] + "'><td>" + data[0] + "</td><td>" + data[1] + "</td><td>" + data[2].toNumber() + "</td><td><button class=\"btn btn-danger btn-sm\" id=\"getOneByCountryName\" onclick=\"App.doDeleteOneByCountryName('" + data[0] + "')\">-</button></td></tr>'");
         })
     }).then(function () {
-      self.setStatus('Transaction complete!')
-      self.refreshBalance()
+      self.setStatus('Transaction complete!');
     }).catch(function (e) {
-      console.log(e)
-      self.setStatus('Error sending coin; see log.')
+      console.log(e);
+      self.setStatus('Error sending coin; see log.');
     })
   },
 
   getTotalCountry: function () {
-    const self = this
-
-    let a
-    let crud
-    this.setStatus('getTotalCountry transaction... (please wait)')
-    CrudApp.deployed().then(function (instance2) {
-      crud = instance2
-      a = crud.getTotalCountries({ from: account })
-      console.log(a)
+    CrudApp.deployed().then(function (instance) {
+      instance.getTotalCountries({from: account}).then(function (data) {
+        alert("등록된 나라는 총 : " + data.toNumber() + "개 입니다.");
+      });
+      console.log(a);
     }).then(function () {
-      self.setStatus('Transaction complete!')
-      self.refreshBalance()
+      this.setStatus('Transaction complete!');
     }).catch(function (e) {
-      console.log(e)
-      self.setStatus('Error sending coin; see log.')
+      console.log(e);
+      this.setStatus('Error sending coin; see log.');
     })
   },
 
   doUpdateOneByCountryName: function () {
-    const self = this
-    const countryName = document.getElementById('countryName').value
-    const leader = document.getElementById('leader').value
+    const self = this;
+    const countryName = document.getElementById('countryName').value;
+    const leader = document.getElementById('leader').value;
 
-    let a
-    let crud
-    this.setStatus('doUpdateOneByContryName transaction... (please wait)')
-    CrudApp.deployed().then(function (instance2) {
-      crud = instance2
-      a = crud.doUpdateLeader(countryName, leader, { from: account })
-      console.log(a)
+    CrudApp.deployed().then(function (instance) {
+      instance.doUpdateLeader(countryName, leader, {from: account});
     }).then(function () {
-      self.setStatus('Transaction complete!')
-      self.refreshBalance()
+      self.setStatus('Transaction complete!');
     }).catch(function (e) {
-      console.log(e)
-      self.setStatus('Error sending coin; see log.')
+      console.log(e);
+      self.setStatus('Error sending coin; see log.');
     })
   },
-  doDeleteOneByCountryName: function () {
-    const self = this
-    const countryName = document.getElementById('countryName').value
-
-    let a
-    let crud
-    this.setStatus('Initiating transaction... (please wait)')
-    CrudApp.deployed().then(function (instance2) {
-      crud = instance2
-      a = crud.doDeleteCountry(countryName, { from: account })
-      console.log(a)
+  doDeleteOneByCountryName: function (countryName) {
+    const self = this;
+    CrudApp.deployed().then(function (instance) {
+      instance.doDeleteCountry(countryName, {from: account});
+      $("#row_" + countryName).hide();
     }).then(function () {
-      self.setStatus('Transaction complete!')
-      self.refreshBalance()
+      self.setStatus('Transaction complete!');
     }).catch(function (e) {
-      console.log(e)
-      self.setStatus('Error sending coin; see log.')
+      console.log(e);
+      self.setStatus('Error sending coin; see log.');
     })
   },
   getMany: function () {
-  const self = this
-
-  let a
-  let crud
-  this.setStatus('Initiating transaction... (please wait)')
-  CrudApp.deployed().then(function (instance2) {
-    crud = instance2
-    crud.getMany({ from: account })
-      .then(function (data) {
-        a = data
-        console.log(a)
-      })
-  }).then(function () {
-    self.setStatus('Transaction complete!')
-    self.refreshBalance()
-  }).catch(function (e) {
-    console.log(e)
-    self.setStatus('Error sending coin; see log.')
-  })
-}
+    const self = this;
+    CrudApp.deployed().then(function (instance) {
+      instance.getMany({from: account})
+        .then(function (item) {
+          item.forEach(function(element) {
+            instance.getOneById(element, {from: account})
+              .then(function (data) {
+                $("#myTable tbody:last").append("<tr id='row_" + data[0] + "'><td>" + data[0] + "</td><td>" + data[1] + "</td><td>" + data[2].toNumber() + "</td><td><button class=\"btn btn-danger btn-sm\" id=\"getOneByCountryName\" onclick=\"App.doDeleteOneByCountryName('" + data[0] + "')\">-</button></td></tr>'");
+              })
+          });
+        })
+    }).then(function () {
+      self.setStatus('Transaction complete!');
+    }).catch(function (e) {
+      console.log(e);
+      self.setStatus('Error sending coin; see log.');
+    })
+  }
 }
 
-window.App = App
+window.App = App;
 
 window.addEventListener('load', function () {
   if (typeof web3 !== 'undefined') {
-    window.web3 = new Web3(web3.currentProvider)
+    window.web3 = new Web3(web3.currentProvider);
   } else {
-    window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'))
+    window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'));
   }
-  App.start()
+  App.start();
 })
--- contracts/CrudApp.sol
+++ contracts/CrudApp.sol
@@ -1,42 +1,40 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.17;
 
 contract CrudApp {
 
-    struct country{
+    struct country {
         string name;
         string leader;
         uint256 population;
     }
 
+    uint[] xs;
+
     country[] public countries;
 
     uint256 public totalCountries;
-
 
     constructor() public {
         totalCountries = 0;
     }
 
-    event CountryEvent(string countryName , string leader, uint256 population);
-
-    event LeaderUpdated(string countryName , string leader);
-
+    event CountryEvent(string countryName, string leader, uint256 population);
+    event LeaderUpdated(string countryName, string leader);
     event CountryDelete(string countryName);
 
 
-    function doInsert( string countryName , string leader , uint256 population) public returns (uint256 total){
-        country memory newCountry = country(countryName , leader, population);
+    function doInsert(string countryName, string leader, uint256 population) public returns (uint256 total){
+        country memory newCountry = country(countryName, leader, population);
         countries.push(newCountry);
         totalCountries++;
-        //emit event
-        emit CountryEvent (countryName, leader, population);
+
+        emit CountryEvent(countryName, leader, population);
         return total;
     }
 
     function doUpdateLeader(string countryName, string newLeader) public returns (bool success){
-        //This has a problem we need loop
-        for(uint256 i =0; i< totalCountries; i++){
-            if(compareStrings(countries[i].name ,countryName)){
+        for (uint256 i = 0; i < totalCountries; i++) {
+            if (compareStrings(countries[i].name, countryName)) {
                 countries[i].leader = newLeader;
                 emit LeaderUpdated(countryName, newLeader);
                 return true;
@@ -45,15 +43,14 @@ contract CrudApp {
         return false;
     }
 
-    function doDeleteCountry(string countryName) public returns(bool success){
+    function doDeleteCountry(string countryName) public returns (bool success){
         require(totalCountries > 0);
-        for(uint256 i =0; i< totalCountries; i++){
-            if(compareStrings(countries[i].name , countryName)){
-                countries[i] = countries[totalCountries-1]; // pushing last into current arrray index which we gonna delete
-                delete countries[totalCountries-1]; // now deleteing last index
-                totalCountries--; //total count decrease
-                countries.length--; // array length decrease
-                //emit event
+        for (uint256 i = 0; i < totalCountries; i++) {
+            if (compareStrings(countries[i].name, countryName)) {
+                countries[i] = countries[totalCountries - 1];
+                delete countries[totalCountries - 1];
+                totalCountries--;
+                countries.length--;
                 emit CountryDelete(countryName);
                 return true;
             }
@@ -62,17 +59,22 @@ contract CrudApp {
     }
 
 
-    function getCountry(string countryName) public view returns(string name , string leader , uint256 population){
-        for(uint256 i =0; i< totalCountries; i++){
-            if(compareStrings(countries[i].name, countryName)){
-                //emit event
-                return (countries[i].name , countries[i].leader , countries[i].population);
+    function getOneByCountryName(string countryName) public view returns (string name, string leader, uint256 population){
+        for (uint256 i = 0; i < totalCountries; i++) {
+            if (compareStrings(countries[i].name, countryName)) {
+                return (countries[i].name, countries[i].leader, countries[i].population);
             }
         }
         revert('country not found');
     }
 
-    function compareStrings (string a, string b)  internal pure returns (bool){
+    function getOneById(uint id) public view returns (string name, string leader, uint256 population){
+        return (countries[id].name, countries[id].leader, countries[id].population);
+
+    }
+
+
+    function compareStrings(string a, string b) internal pure returns (bool){
         return keccak256(a) == keccak256(b);
     }
 
@@ -81,7 +83,18 @@ contract CrudApp {
         return countries.length;
     }
 
-    function getCountry() public view returns(country[]){
-        return countries;
+    function A() {
+        xs.push(100);
+        xs.push(200);
+        xs.push(300);
     }
+
+    function getMany() public view returns (uint[]) {
+        for (uint256 i = 0; i < totalCountries; i++) {
+            xs.push(i);
+        }
+
+        return xs;
+    }
+
 }
