b1c319689a97e65f5a4101a006ded15cb08dbc43
dev.lkb <dev.lkb@iventory.co.kr>
2019-12-17 16:27:47 +0900

    port change

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -141,7 +141,7 @@ window.addEventListener('load', function () {
   if (typeof web3 !== 'undefined') {
     window.web3 = new Web3(web3.currentProvider);
   } else {
-    window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:7545'));
+    window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'));
   }
   App.start();
 })
--- truffle.js
+++ truffle.js
@@ -7,8 +7,10 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
-      network_id: "*" // Match any network id
+      port: 8545,
+      gas: 6721975,
+      network_id: "*"
+
     }
   }
 };
