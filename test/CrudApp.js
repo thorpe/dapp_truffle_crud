44b110f8e75b437b05336f2da673928eb55f3864
koangbok@gmail.com <gkfls!@34>
2019-12-24 02:28:18 +0900

    sdf

--- test/CrudApp.js
+++ test/CrudApp.js
@@ -6,7 +6,9 @@ contract('CrudApp', function() {
 
     const crudApp = await CrudApp.deployed();
     await  crudApp.doInsert('USA1', 'Trumpq', 30000000);
+    const count = await crudApp.getTotalCountries();
 
+    assert(count.toNumber()  ===  1);
   });
 
 });
